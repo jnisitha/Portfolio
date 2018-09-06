d6c8af9269747721665ef6b9075586b6d41c5b17
nisi.jayatilleka@mail.utoronto.ca <nisi.jayatilleka@mail.utoronto.ca>
2018-09-06 17:34:56 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/jekyll/update/2018/08/31/welcome-to-jekyll.html",
+        "url": "/jekyll/update/welcome-to-jekyll/",
         "teaser":null},{
         "title": "First Mech post",
         "excerpt":"This is project submarine ","categories": [],
         "tags": [],
-        "url": "/2018/09/04/first-mech-post.html",
+        "url": "/first-mech-post/",
         "teaser":null}]
